e0552d9bc9017455af6a35718e9aa9025c868599
MichalFiedor <michal.fiedor92@gmail.com>
2020-11-04 17:44:53 +0100

    app configuration

---
+++ src/test/java/pl/michalfiedor/cvbuilder/CvbuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.michalfiedor.cvbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvbuilderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
